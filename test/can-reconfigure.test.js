3236ea32b71db781ea0daa4a346dfd82942282bd
Eliomar Rodríguez <rodriguez.elio.97@gmail.com>
2023-01-25 00:03:04 -0600

    Improve test

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -1,31 +1,6 @@
 /* eslint-disable no-undef */
 import { describe, expect, it } from 'vitest'
-
-const canConfigure = (from, to) => {
-    if (from === undefined) throw new Error('from is required')
-    if (typeof from !== 'string') throw new Error('from is not a string')
-    if (typeof to !== 'string') throw new Error('to is not a string')
-    if (from.length !== to.length) return false
-
-    const isSameLength = from.length === to.length
-    if (!isSameLength) return false
-
-    const hasSameUniqueLetters = new Set(from).size === new Set(to).size
-    if (!hasSameUniqueLetters) return false
-
-    const transformations = {}
-    for (let i = 0; i < from.length; i++) {
-        const fromLetter = from[i]
-        const toLetter = to[i]
-
-        const storedLetter = transformations[fromLetter]
-        if (storedLetter && storedLetter !== toLetter) return false
-
-        transformations[fromLetter] = toLetter
-    }
-
-    return true
-}
+import { canConfigure } from '../src/can-reconfigure'
 
 describe('canReconfigure', () => {
     it('should be a function', () => {
